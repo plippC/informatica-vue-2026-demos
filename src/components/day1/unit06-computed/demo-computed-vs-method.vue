7541021481a89378bc223b075f917b7a1922f40e
lipp.p <patricia.lipp@cornelsen.de>
2026-08-30 20:33:25 +0200

    improve computed sample

--- src/components/day1/unit06-computed/demo-computed-vs-method.vue
+++ src/components/day1/unit06-computed/demo-computed-vs-method.vue
@@ -3,8 +3,15 @@ import { ref, computed } from 'vue'
 const price = ref(10)
 const quantity = ref(2)
 
-const total = computed(() => price.value * quantity.value)
+const name = ref('')
+
+const total = computed(() => {
+  console.log('calculate computed total')
+  return price.value * quantity.value
+})
+
 function totalFn() {
+  console.log('calculate totalFn')
   return price.value * quantity.value
 }
 </script>
@@ -14,4 +21,6 @@ function totalFn() {
   ×
   <input v-model.number="quantity" type="number" />
   = {{ total }} ({{ totalFn() }})
+  <hr />
+  <input v-model="name" placeholder="independent name" /> {{ name }}
 </template>
