3d32b9f53fe1823fc127dfb80930b13271006ff2
lipp.p <patricia.lipp@cornelsen.de>
2026-08-30 20:56:52 +0200

    add documentation

--- src/components/day1/unit08-onMounted-watch/demo-watch.vue
+++ src/components/day1/unit08-onMounted-watch/demo-watch.vue
@@ -19,4 +19,11 @@ watch(name, (val) => {
 <template>
   <input v-model="name" placeholder="Type your name…" />
   <input v-model="place" placeholder="Type a place…" />
+  <p>
+    More about the <code>localStorage</code> API see <a
+      href="https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage">MDN Documentation</a>
+  </p>
+  <p>
+    More about Vue Lifecycle Hooks see <a href="https://vuejs.org/guide/essentials/lifecycle.html">Vue Documentation</a>
+  </p>
 </template>
