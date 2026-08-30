4b2c6e9019f0128498a2babe81731abe72f3378c
lipp.p <patricia.lipp@cornelsen.de>
2026-08-30 18:53:16 +0200

    simplify demo

--- src/components/day1/unit03-template-syntax/demo1-v-bind.vue
+++ src/components/day1/unit03-template-syntax/demo1-v-bind.vue
@@ -7,15 +7,10 @@ const isActive = ref(false)
   <h1>Text-Content</h1>
   isActive = {{ isActive }} = {{ isActive ? 'active' : 'inactive' }}
   <h1>Attributes</h1>
-  <button :class="{ active: isActive }" @click="isActive = !isActive">Toggle</button>
+  <button :class="{ isActive }" @click="isActive = !isActive">Toggle</button>
 </template>
 
 <style scoped>
-.active {
-  background-color: rgb(21, 177, 21);
-  color: white;
-}
-
 .isActive {
   background-color: blue;
 }
